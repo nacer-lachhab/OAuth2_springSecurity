2ce17e65323b501847c98358318a93c674b53f28
lachhab nacer <lachhab.nacer@gmail.com>
2022-10-13 01:04:36 +0100

    jwt implementation added+tokenStore

--- src/main/java/laurSpilca/tuto/nacer/config/AuthServerConfig.java
+++ src/main/java/laurSpilca/tuto/nacer/config/AuthServerConfig.java
@@ -1,12 +1,17 @@
 package laurSpilca.tuto.nacer.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer //active le seveur des authorisations.
@@ -40,11 +45,24 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter{
 			   .authorizedGrantTypes("client_credentials");
 	}
 
+	
+	@Bean
+	public TokenStore tokenStore() {
+		return new JwtTokenStore(converter());
+	}
+	
+	@Bean
+	public JwtAccessTokenConverter converter() {
+		return new JwtAccessTokenConverter();
+	}
+	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
 		
 		//liaison des users aux AuthorizationServerConfiguration.
-		endpoints.authenticationManager(authenticationManager);
+		endpoints.authenticationManager(authenticationManager)
+				 .tokenStore(tokenStore())
+				 .accessTokenConverter(converter());
 		
 	}
 	
